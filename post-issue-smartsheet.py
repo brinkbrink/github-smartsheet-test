00123f258e07a4591941a01bdd5c977cbd37f0b4
brinkbrink <brinkbrink@gmail.com>
2024-05-08 13:56:55 -0700

    matching json values to appropriate columns

--- post-issue-smartsheet.py
+++ post-issue-smartsheet.py
@@ -26,6 +26,9 @@ response = requests.get('https://api.github.com/repos/brinkbrink/github-smartshe
                                  'X-GitHub-Api-Version': '2022-11-28'})
 issues = response.json()
 
+repo_url = issues['repository_url']
+print(repo_url)
+print(repo_url[40:])
 
 # POST request to Smartsheet API
 smartsheet_response = requests.post(
@@ -42,23 +45,28 @@ smartsheet_response = requests.post(
         'cells': [
             {
             'columnId': 5558737690382212,
-            'displayValue': 'test1',
+            'displayValue': 'title',
             'value': issues['title']
             },
             {
             'columnId': 3306937876696964,
-            'displayValue': 'test1',
-            'value': 'IS THIS'
+            'displayValue': 'repo url',
+            'value': repo_url[40:]
             },
             {
             'columnId': 7810537504067460,
-            'displayValue': 'test1',
-            'value': 'working for'
+            'displayValue': 'priority',
+            'value': 'priority pull from PBI'
             },
             {
             'columnId': 2181037969854340,
-            'displayValue': 'test1',
-            'value': '??'
+            'displayValue': 'assignee',
+            'value': issues['assignee']['login']
+            },
+            {
+            'columnId': 6684637597224836,
+            'displayValue': 'index',
+            'value': issues['number']
             }
         ],
         'modifiedAt': '2019-08-24T14:15:22Z',
