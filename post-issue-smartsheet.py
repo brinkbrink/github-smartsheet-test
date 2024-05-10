71acdf3f1d35ff0463593234d066bdd73b28d905
brinkbrink <brinkbrink@gmail.com>
2024-05-10 11:55:57 -0700

    incrementing with github var

--- post-issue-smartsheet.py
+++ post-issue-smartsheet.py
@@ -5,6 +5,7 @@ import os
 
 SMART_ACCESS_TOKEN = os.environ['SMART_ACCESS_TOKEN']
 GITHUB_ACCESS_TOKEN = os.environ['GH_ACCESS_TOKEN']
+ISSUE_NUM = os.environ.get('ISSUE_NUM', 'default_value')  # 'default_value' is optional
 
 # Initialize client. Uses the API token in the environment variable 'SMARTSHEET_ACCESS_TOKEN'
 smart = smartsheet.Smartsheet(SMART_ACCESS_TOKEN)
@@ -15,10 +16,14 @@ smart.errors_as_exceptions(True)
 logging.basicConfig(filename='rwsheet.log', level=logging.INFO)
 
 # GET request to GitHub API
-response = requests.get('https://api.github.com/repos/brinkbrink/github-smartsheet-test/issues/13',
-                        headers={'Authorization': GITHUB_ACCESS_TOKEN, 
-                                 'Content-Type': 'application/vnd.github+json',
-                                 'X-GitHub-Api-Version': '2022-11-28'})
+response = requests.get(
+    f'https://api.github.com/repos/brinkbrink/github-smartsheet-test/issues/{ISSUE_NUM}',
+    headers={
+        'Authorization': f'token {GITHUB_ACCESS_TOKEN}', 
+        'Content-Type': 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+)
 issues = response.json()
 
 # For use below--in order to truncate url to use as repo name
@@ -27,7 +32,10 @@ repo_url = issues['repository_url']
 # POST request to Smartsheet API
 smartsheet_response = requests.post(
     'https://api.smartsheet.com/2.0/sheets/2342839996338052/rows',
-    headers={'Authorization': f'Bearer {SMART_ACCESS_TOKEN}', 'Content-Type': 'application/json'},
+    headers={
+        'Authorization': f'Bearer {SMART_ACCESS_TOKEN}', 
+        'Content-Type': 'application/json'
+    },
     json={
         'sheetId': 2342839996338052,
         'accessLevel': 'OWNER',
@@ -36,29 +44,30 @@ smartsheet_response = requests.post(
         },
         'cells': [
             {
-            'columnId': 5558737690382212,
-            'displayValue': 'title',
-            'value': issues['title']
+                'columnId': 5558737690382212,
+                'displayValue': 'title',
+                'value': issues['title']
             },
             {
-            'columnId': 3306937876696964,
-            'displayValue': 'repo url',
-            'value': repo_url[40:]
+                'columnId': 3306937876696964,
+                'displayValue': 'repo url',
+                'value': repo_url[40:]
             },
             {
-            'columnId': 7810537504067460,
-            'displayValue': 'priority',
-            'value': 'priority pull from PBI'
+                'columnId': 7810537504067460,
+                'displayValue': 'priority',
+                'value': 'priority pull from PBI'
             },
             {
-            'columnId': 2181037969854340,
-            'displayValue': 'assignee',
-            'value': issues['assignee']['login'] # TODO: create logic in case this is empty
+                'columnId': 2181037969854340,
+                'displayValue': 'assignee',
+                'value': issues.get('assignee', {}).get('login', 'No Assignee')  # Safely access assignee
             },
             {
-            'columnId': 6684637597224836,
-            'displayValue': 'index',
-            'value': issues['number']
+                'columnId': 6684637597224836,
+                'displayValue': 'index',
+                'value': issues['number']
             }
         ]
-        })
+    }
+)
